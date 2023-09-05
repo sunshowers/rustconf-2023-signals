5de00b70109e4af4d3138af184f18a54d9b47473
Rain <rain@sunshowers.io>
2023-09-04 23:42:15 -0700

    simplify release.yml, add another readme

---
+++ download-manager/README.md
@@ -0,0 +1,3 @@
+# download-manager
+
+See [the parent README](../README.md#download-manager) for more information.
--- download-manager/src/lib.rs
+++ download-manager/src/lib.rs
@@ -1,3 +1,7 @@
+//! lib.rs for the download-manager binary.
+//!
+//! The logic is implemented in command.rs -- head there to start.
+
 mod command;
 mod db;
 mod manifest;
